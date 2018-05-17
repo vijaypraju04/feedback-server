765e98d91959c351e117ff4f8578ef3018637740
Vijay Raju <vijaypraju04@gmail.com>
2018-05-17 14:10:46 -0700

    oauth id

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+//CLIENT ID
+//CLIENT SECRET
+
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+ // we only want the strategy part
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(
+  new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+      console.log(accessToken)
+    }
+  )
+)
+// creates a new instance of GoogleStrategy passport
+// tells application we want to authenticate users with google
+
+// app.get('/', (req, res) => {
+//   res.send({ bye: 'buddy' });
+// });
 
 const PORT = process.env.PORT || 5000
 // runs locally if no environment variable defined
