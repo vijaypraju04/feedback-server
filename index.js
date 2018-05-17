ccd819a07c140b151690fd5681004e820e52d634
Vijay Raju <vijaypraju04@gmail.com>
2018-05-16 19:17:48 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000
+// runs locally if no environment variable defined
+// meaning heroku is not running it defaults to
+// a server on our machines
+app.listen(PORT);
