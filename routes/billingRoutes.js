7894878f7a2c3fb44b7e668008562f8847a44aa6
Vijay Raju <vijaypraju04@gmail.com>
2018-05-29 22:06:35 -0700

    added login error

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+      // unauthorized or forbidden status code
+    }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
