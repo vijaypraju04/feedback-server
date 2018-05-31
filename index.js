55bdc9c8f604eda3f35261bffdcb5e3eed1f0c0d
Vijay Raju <vijaypraju04@gmail.com>
2018-05-30 17:17:16 -0700

    survey model added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 // we need to make sure our User model is defined before using passport ^^
