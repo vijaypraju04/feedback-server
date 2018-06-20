8a1b7fde6b664493dafda95ae80b23abedaed6a3
Vijay Raju <vijaypraju04@gmail.com>
2018-06-19 17:36:21 -0700

    fixed survey form review

--- feedback-client/src/components/surveys/SurveyForm.js
+++ feedback-client/src/components/surveys/SurveyForm.js
@@ -1,7 +1,6 @@
 // SurveyForm shows a form for a user to add input
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { actionBindCreator } from 'redux';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
--- feedback-client/src/components/surveys/SurveyFormReview.js
+++ feedback-client/src/components/surveys/SurveyFormReview.js
@@ -7,7 +7,7 @@ const SurveyFormReview = ({ onCancel }) => {
     <div>
       <h5>Please confirm your entries</h5>
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
-        Back
+        Cancel
       </button>
     </div>
   );
